cd1a61dea337d93fdb19a58530c927843135aa5d
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-21 18:19:16 +0530

    PINCODE Valid 6 Digit

---
+++ PINCODE_Regex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PINCODE_Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter PINCODE : ");
+            long pin = Convert.ToInt64(Console.ReadLine());
+
+            Regex code = new Regex(("^[1-9]{1}[0-9]{5}$"));
+            bool matches = code.IsMatch(Convert.ToString(pin));
+
+            if (matches == true)
+            {
+                Console.WriteLine("Pincode is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Pincode is invalid");
+            }
+        }
+    }
+}
